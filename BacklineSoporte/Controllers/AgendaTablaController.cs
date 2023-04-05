71f7b4ac471ba367756be7707bb37895f20eccdf
Alexis Cheuquiante <alexischeuquiante@outlook.com>
2023-04-05 18:16:32 -0400

    Se modifica agenda

--- BacklineSoporte/Controllers/AgendaTablaController.cs
+++ BacklineSoporte/Controllers/AgendaTablaController.cs
@@ -9,9 +9,24 @@ namespace BacklineSoporte.Controllers
     public class AgendaTablaController : Controller
     {
         // GET: AgendaTabla
-        public ActionResult Index()
+        public ActionResult Index(string limpiar)
         {
-            return View();
+            BacklineSoporte.Models.AgendaModel modelo = new Models.AgendaModel();
+            Entity.Filtro filtro = new Entity.Filtro();
+
+            if (limpiar != null)
+            {
+                DateTime inicioSemana = Utiles.ObtenerInicioSemana(DateTime.Now);
+                DateTime terminoSemana = Utiles.ObtenerTerminoSemana(DateTime.Now);
+
+                Session["FiltroTareaDesde"] = Utiles.ReversaFecha(inicioSemana);
+                Session["FiltroTareaHasta"] = Utiles.ReversaFecha(terminoSemana);
+                filtro.FechaDesde = Utiles.FechaObtenerMinimo(inicioSemana);
+                filtro.FechaHasta = Utiles.FechaObtenerMaximo(terminoSemana);
+                modelo.ListaAgenda = DAL.TareaDAL.ObtenerTarea(filtro);
+                Session["registrosEncontrados"] = modelo.ListaAgenda;
+            }
+            return View(modelo);
         }
     }
 }
